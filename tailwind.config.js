894d9cdbf659566843187183cd7fba17ab14f52f
Hilmi Alfaruqy <halfaruqy@gmail.com>
2024-05-06 07:16:57 +0700

    add style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,34 @@ module.exports = {
       fontFamily: {
         rubik: ["Rubik", "sans-serif"],
       },
+      colors: {
+        'primary': {
+          50: '#fffdea',
+          100: '#fff8c5',
+          200: '#fff185',
+          300: '#ffe246',
+          400: '#ffd11b',
+          500: '#ffb000',
+          600: '#e28600',
+          700: '#bb5d02',
+          800: '#984808',
+          900: '#7c3b0b',
+          950: '#481d00',
+        },
+        'secondary': {
+          50: '#e8f3ff',
+          100: '#d5e9ff',
+          200: '#b3d4ff',
+          300: '#86b5ff',
+          400: '#5687ff',
+          500: '#305aff',
+          600: '#0c27ff',
+          700: '#031dff',
+          800: '#061ecd',
+          900: '#10259f',
+          950: '#050a30',
+        },
+      }
     },
     screens: {
       // => @media (min-width: ... px) { ... }
